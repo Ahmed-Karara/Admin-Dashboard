6bbe802df1e7fd94f7ce55444ac4387669db791a
Ahmed Karara <110180686+Ahmed-Karara@users.noreply.github.com>
2023-01-11 16:37:51 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
-import './App.css';
-import Sidebar from './components/Sidebar/Sidebar';
-import MainContenet from './components/MainContenet/MainContenet';
-import Orders from './components/Orders/Orders';
-import Products from './components/Products/Products';
-import RecentActivity from './components/RecentActivity/RecentActivity';
-import Analytics from './components/Analytics/Analytics';
-import Customers from './components/Customers/Customers';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import "./App.css";
+import Sidebar from "./components/Sidebar/Sidebar";
+import MainContenet from "./components/MainContenet/MainContenet";
+import Orders from "./components/Orders/Orders";
+import Products from "./components/Products/Products";
+import RecentActivity from "./components/RecentActivity/RecentActivity";
+import Analytics from "./components/Analytics/Analytics";
+import Customers from "./components/Customers/Customers";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-	return (
-		<div className='App'>
-			<div className='container'>
-				<BrowserRouter>
-					<Sidebar />
-					<Routes>
-						<Route index path='/' element={<MainContenet />} exact></Route>
-						<Route path='/Dashboard' element={<MainContenet />}></Route>
-						<Route path='Orders' element={<Orders />}></Route>
-						<Route path='Customers' element={<Customers />}></Route>
-						<Route path='Products' element={<Products />}></Route>
-						<Route path='Analytics' element={<Analytics />}></Route>
-					</Routes>
-				</BrowserRouter>
-				<RecentActivity />
-			</div>
-		</div>
-	);
+  return (
+    <div className="App">
+      <div className="container">
+        <BrowserRouter>
+          <Sidebar />
+          <Routes>
+            <Route path="/*" element={<MainContenet />}></Route>
+            <Route path="Orders" element={<Orders />}></Route>
+            <Route path="Customers" element={<Customers />}></Route>
+            <Route path="Products" element={<Products />}></Route>
+            <Route path="Analytics" element={<Analytics />}></Route>
+          </Routes>
+        </BrowserRouter>
+        <RecentActivity />
+      </div>
+    </div>
+  );
 }
 
 export default App;
